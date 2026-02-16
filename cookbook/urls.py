99cd62fe0e4ddbd0a209e64c419c9205ce88bf9d
Marek Skrzypiński <skrzypinski009@gmail.com>
2026-02-17 00:01:47 +0100

    fix: change app urls

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("cookbook_site.urls")),
+    path("api/", include("cookbook_site.urls")),
     path("admin/", admin.site.urls),
 ]
--- cookbook_site/urls.py
+++ cookbook_site/urls.py
@@ -1,6 +1,15 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+from .views import RecipeViewSet, IngredientViewSet, RateViewSet, RegisterView
+
+router = DefaultRouter()
+router.register(r"recipes", RecipeViewSet)
+router.register(r"ingredients", IngredientViewSet)
+router.register(r"rates", RateViewSet)
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("api/", include(router.urls)),
+    path("api/register/", RegisterView.as_view(), name="register"),
+    path("api/login/", obtain_auth_token, name="login"),
 ]
